fe4194c78d015d32dfbe85094a96ba56ca3436f6
Guidedlegend <legendt017@gmail.com>
2025-04-11 15:27:35 +0530

    Update DeleteProductResponseDTO.java

--- src/main/java/com/hello/amazon/DeleteProductResponseDTO.java
+++ src/main/java/com/hello/amazon/DeleteProductResponseDTO.java
@@ -6,7 +6,7 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class DeleteProductResponseDTO {
-	private String message;
+    private String message;
     private EProduct deletedProduct;
 
 }
